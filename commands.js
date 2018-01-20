39cb7f8b47577c3a90199a3965abdd02a90c2e13
Jason Rosso <jasonrosso@gmail.com>
2018-01-20 16:28:35 -0500

    grep function added

--- commands.js
+++ commands.js
@@ -142,6 +142,17 @@ commands.curl =(stdin,url, done)=>{ ///COME BACK TO THIS/
     }
 
 }
+commands.grep =(stdin,matchString,done)=>{
+    const matcher = new RegExp(matchString, 'g')
+    const matches = stdin
+    .split('\n')
+    .filter(line=>matcher.test(line))
+    .map(line=>{
+        return line.replace(matcher, match =>chalk.green(match))
+    })
+    .join('\n');
+    done(matches);
+}
 
 
 module.exports=commands
